d8bc50b87365b03008329c8021552d45db0ba818
= <=>
2025-01-22 15:39:03 +0400

    feat: Create folder simple structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import './App.css';
-
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <div>APP</div>
   );
 };
 
