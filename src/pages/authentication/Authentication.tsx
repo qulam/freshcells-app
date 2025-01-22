388897c11dcc212fc003338551a7f0197b03d361
= <=>
2025-01-22 19:58:48 +0400

    feat: Finish login and authentication wrapper page

--- src/pages/authentication/Authentication.tsx
+++ src/pages/authentication/Authentication.tsx
@@ -1,12 +1,27 @@
+import { Paper, Title } from '@mantine/core';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router';
 import { Error } from '@app/components';
+import classes from './Authentication.module.scss';
 
 const Authentication = () => {
   return (
     <ErrorBoundary fallback={<Error />}>
-      <h1>Authentication</h1>
-      <Outlet />
+      <div className={classes.wrapper}>
+        <Paper className={classes.form} radius={0} p={30}>
+          <Title
+            order={2}
+            className={classes.title}
+            ta="center"
+            mt="md"
+            mb={50}
+          >
+            Welcome back to Fresh Cells!
+          </Title>
+
+          <Outlet />
+        </Paper>
+      </div>
     </ErrorBoundary>
   );
 };
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,42 @@
-import { Text } from '@mantine/core';
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  PasswordInput,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Link } from 'react-router';
 import { Error } from '@app/components';
 
 const Login = () => {
   return (
     <ErrorBoundary fallback={<Error />}>
-      <Text>Login</Text>
+      <TextInput
+        label="Email address"
+        placeholder="hello@gmail.com"
+        size="md"
+        defaultValue="test@freshcells.de"
+      />
+      <PasswordInput
+        label="Password"
+        placeholder="Your password"
+        mt="md"
+        size="md"
+        defaultValue="KTKwXm2grV4wHzW"
+      />
+      <Checkbox label="Keep me logged in" mt="xl" size="md" />
+      <Button fullWidth mt="xl" size="md">
+        Login
+      </Button>
+
+      <Text ta="center" mt="md">
+        Don&apos;t have an account?{' '}
+        <Anchor component={Link} to="/authentication/register" fw={700}>
+          Register
+        </Anchor>
+      </Text>
     </ErrorBoundary>
   );
 };
