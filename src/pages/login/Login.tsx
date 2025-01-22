5756ca968c9b2cbb40ce46b92a604a34466363da
= <=>
2025-01-22 21:26:43 +0400

    feat: Basic form implementation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,4 @@
+import { gql, useMutation } from '@apollo/client';
 import {
   Anchor,
   Button,
@@ -6,30 +7,69 @@ import {
   Text,
   TextInput,
 } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Link } from 'react-router';
 import { Error } from '@app/components';
 
+const DEFAULT_PASSWORD = 'test@freshcells.de';
+const DEFAULT_EMAIL = 'test@freshcells.de';
+
+const LOGIN = gql`
+  # Increments a back-end counter and gets its resulting value
+  mutation IncrementCounter {
+    currentValue
+  }
+`;
+
 const Login = () => {
+  const [login, { loading, error }] = useMutation(LOGIN);
+
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      email: DEFAULT_EMAIL,
+      password: DEFAULT_PASSWORD,
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+  });
+
+  const handleOnSubmit = (values: typeof form.values) => {
+    const { email, password } = values;
+    console.log('email', values.email);
+    console.log('passowrd', values.password);
+    login({ variables: { password, email } });
+  };
+
+  if (loading) return 'loading...';
+  if (error) return 'error...';
+
   return (
     <ErrorBoundary fallback={<Error />}>
-      <TextInput
-        label="Email address"
-        placeholder="hello@gmail.com"
-        size="md"
-        defaultValue="test@freshcells.de"
-      />
-      <PasswordInput
-        label="Password"
-        placeholder="Your password"
-        mt="md"
-        size="md"
-        defaultValue="KTKwXm2grV4wHzW"
-      />
-      <Checkbox label="Keep me logged in" mt="xl" size="md" />
-      <Button fullWidth mt="xl" size="md">
-        Login
-      </Button>
+      <form onSubmit={form.onSubmit(handleOnSubmit)}>
+        <TextInput
+          label="Email address"
+          placeholder="hello@gmail.com"
+          size="md"
+          key={form.key('email')}
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          mt="md"
+          size="md"
+          key={form.key('password')}
+          {...form.getInputProps('password')}
+        />
+        <Checkbox label="Keep me logged in" mt="xl" size="md" />
+        <Button fullWidth mt="xl" size="md" type="submit">
+          Login
+        </Button>
+      </form>
 
       <Text ta="center" mt="md">
         Don&apos;t have an account?{' '}
