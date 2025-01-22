1ef07693f591fe1edc06e597bdc534adae05bc79
= <=>
2025-01-22 14:08:09 +0400

    feat: Test husky before commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 
 const App = () => {
+    const a = 1;
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
+
+
+
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
